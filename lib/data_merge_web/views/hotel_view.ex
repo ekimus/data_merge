a9fcdd1c961a3fa4eca15d49b7e92bf88ec79e98
Paolo Reyes Balleza <paolo.balleza@gmail.com>
2020-01-14 16:27:55 +0800

    Fix nil handling and atom conversion

--- lib/data_merge_web/views/hotel_view.ex
+++ lib/data_merge_web/views/hotel_view.ex
@@ -1,5 +1,6 @@
 defmodule DataMergeWeb.HotelView do
   use DataMergeWeb, :view
+  require Logger
 
   alias DataMerge.Hotels.Hotel
 
@@ -8,23 +9,26 @@ defmodule DataMergeWeb.HotelView do
   end
 
   def render("hotel.json", %{hotel: %Hotel{} = hotel}) do
+    lat = if is_nil(hotel.location.lat), do: nil, else: Decimal.to_float(hotel.location.lat)
+    lng = if is_nil(hotel.location.lng), do: nil, else: Decimal.to_float(hotel.location.lng)
+
     %{
       id: hotel.id,
       destination_id: hotel.destination_id,
       name: hotel.name,
       description: hotel.description,
       location: %{
-        lat: Decimal.to_float(hotel.location.lat),
-        lng: Decimal.to_float(hotel.location.lng),
+        lat: lat,
+        lng: lng,
         address: hotel.location.address,
         city: hotel.location.city,
         country: hotel.location.country
       },
-      amenities: Enum.group_by(hotel.amenities, &String.to_existing_atom(&1.type), & &1.amenity),
+      amenities: Enum.group_by(hotel.amenities, &String.to_atom(&1.type), & &1.amenity),
       images:
         Enum.group_by(
           hotel.images,
-          &String.to_existing_atom(&1.type),
+          &String.to_atom(&1.type),
           &%{link: &1.link, description: &1.description}
         ),
       booking_conditions: Enum.map(hotel.booking_conditions, & &1.booking_condition)
